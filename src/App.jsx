ac7b8766859702fc2a4f5b001ce9dcc4080928de
FAZO-LOGISTICA <gustavo.oliva.miranda@gmail.com>
2026-07-23 16:32:55 -0400

    Auto-update API_URL -> https://classroom-operator-colored-carry.trycloudflare.com

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
 });
 
-const API_URL = "https://proposition-sage-individuals-conduct.trycloudflare.com";
+const API_URL = "https://classroom-operator-colored-carry.trycloudflare.com";
 const CLOUDINARY_CLOUD = "drhceyh7g";
 const CLOUDINARY_PRESET = "bateacontrol";
 
