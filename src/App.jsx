8b7690f677e3edfb85cf36802ecfa9a6c96b6c2a
FAZO-LOGISTICA <gustavo.oliva.miranda@gmail.com>
2026-07-23 13:29:10 -0400

    Auto-update API_URL -> https://grammar-helpful-horizon-guys.trycloudflare.com

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
 });
 
-const API_URL = "https://brave-studies-under-col.trycloudflare.com";
+const API_URL = "https://grammar-helpful-horizon-guys.trycloudflare.com";
 const CLOUDINARY_CLOUD = "drhceyh7g";
 const CLOUDINARY_PRESET = "bateacontrol";
 
