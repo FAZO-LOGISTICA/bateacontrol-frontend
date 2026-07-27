adc857e5b240e210758a0a4c7c41234fb5b54c50
FAZO-LOGISTICA <gustavo.oliva.miranda@gmail.com>
2026-07-27 11:38:14 -0400

    Auto-update API_URL -> https://sufficiently-weekends-identity-divide.trycloudflare.com

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
 });
 
-const API_URL = "https://gates-advantage-share-sunrise.trycloudflare.com";
+const API_URL = "https://sufficiently-weekends-identity-divide.trycloudflare.com";
 const CLOUDINARY_CLOUD = "drhceyh7g";
 const CLOUDINARY_PRESET = "bateacontrol";
 
