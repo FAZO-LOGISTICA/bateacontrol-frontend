28cc662710864bac56368079b1585ca189344929
FAZO-LOGISTICA <gustavo.oliva.miranda@gmail.com>
2026-08-25 12:06:15 -0400

    Auto-update API_URL -> https://a764-200-50-126-98.ngrok-free.app

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
 });
 
-const API_URL = "https://ff7d-200-50-126-98.ngrok-free.app";
+const API_URL = "https://a764-200-50-126-98.ngrok-free.app";
 const CLOUDINARY_CLOUD = "drhceyh7g";
 const CLOUDINARY_PRESET = "bateacontrol";
 
