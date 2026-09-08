575c35b46511707a70e3f77f074ef3684c9f6666
FAZO-LOGISTICA <gustavo.oliva.miranda@gmail.com>
2026-09-08 16:34:50 -0300

    Auto-update API_URL -> https://eac5-2800-150-14c-465-40a8-9f87-d4c9-aefa.ngrok-free.app

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
 });
 
-const API_URL = "https://30b1-200-50-126-98.ngrok-free.app";
+const API_URL = "https://eac5-2800-150-14c-465-40a8-9f87-d4c9-aefa.ngrok-free.app";
 const CLOUDINARY_CLOUD = "drhceyh7g";
 const CLOUDINARY_PRESET = "bateacontrol";
 
